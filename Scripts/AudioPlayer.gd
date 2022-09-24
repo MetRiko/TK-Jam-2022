ac2d67d87407681eef74e42d0d7a452903991f50
Dualithe <dualithe@gmail.com>
2022-09-24 15:58:53 +0200

    Add sound management with sample sounds

--- Scripts/AudioPlayer.gd
+++ Scripts/AudioPlayer.gd
@@ -1,13 +1,21 @@
 extends Node
 
 onready var maxVol
+var currStream
+export(Array,AudioStreamSample) var streamList 
 
 func _process(delta):
 	if $AudioStreamPlayer.playing == false:
 		$AudioStreamPlayer.play()
 		maxVol = $AudioStreamPlayer.volume_db
-	pass
 
 func changeVolume(volume):
 	volume = volume*100/maxVol
 	$AudioStreamPlayer.volume_db = volume
+
+func playSound(soundName):
+	for streams in streamList:
+		if(soundName == streams.resource_name):
+			currStream = streams
+	$EffectsAudioStreamPlayer.stream = currStream
+	$EffectsAudioStreamPlayer.play()
