7517a0aa35b120186d676b622d7bceb98b6da8ba
aleksander-czeszyk-tsg <aleksander.czeszyk@tensquaregames.com>
2022-09-24 06:33:50 +0200

    rotating movement added

--- Scenes/SnakeHead.gd
+++ Scenes/SnakeHead.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 
 var direction: Vector2 = Vector2.RIGHT
-var velocity: Vector2 = Vector2(10.0, 0.0)
+var velocity: int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,20 +11,18 @@ func _ready():
 
 func _process(delta):
 	update_direction_by_input()
-	var velocity_calc: Vector2 = get_linear_velocity()
-	velocity = move_and_slide(velocity_calc, Vector2.UP)
+	var velocity_vec: Vector2 = get_linear_velocity()
+	velocity_vec = move_and_slide(velocity_vec, Vector2.UP)
 
 
 func update_direction_by_input():
 	if Input.is_action_pressed("ui_right"):
-		direction.rotated(deg2rad(20))
-		print(direction)
+		direction = direction.rotated(deg2rad(3))
 	if Input.is_action_pressed("ui_left"):
-		direction.rotated(deg2rad(-20))
-		print(direction)
+		direction = direction.rotated(deg2rad(-3))
 
 func get_linear_velocity():
 	var linear_velocity: Vector2
-	linear_velocity.x = direction.x * velocity.x
-	linear_velocity.y = direction.y * velocity.y
+	linear_velocity.x = direction.x * velocity
+	linear_velocity.y = direction.y * velocity
 	return linear_velocity
