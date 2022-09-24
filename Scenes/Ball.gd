9878746b289ac1e8e34a00b0265ae4b05357b9ba
Dualithe <dualithe@gmail.com>
2022-09-24 14:57:37 +0200

    Add ball worky

--- Scenes/Ball.gd
+++ Scenes/Ball.gd
@@ -1,14 +1,22 @@
 extends KinematicBody2D
 
-var velocity = 200.0
+var velocity = 3.0
 var dirVec = Vector2.ZERO
 
 func castBall():
-	dirVec = Vector2(rand_range(-0.5,0.5),1)
+	dirVec = Vector2(1,-1)
+	dirVec.x = dirVec.x * rand_range(-1,1) *0
 
 func _physics_process(delta):
-	move_and_collide(dirVec*velocity)
+	var collision = move_and_collide(dirVec*velocity)
+	if collision:
+		var reflect = collision.remainder.bounce(collision.normal)
+		dirVec = dirVec.bounce(collision.normal)
+		move_and_collide(reflect)
+		if collision.collider.name.match('*TetrisBlock*'):
+			collision.collider.destroy()
 
 func _ready():
+	randomize()
 	castBall()
 
